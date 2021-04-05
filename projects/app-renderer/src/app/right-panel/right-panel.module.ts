d821c9822df69eb44ce55a21d725fda8c8d0feeb
Thomas Marcelin <thomas.marcelin.07@gmail.com>
2021-04-05 14:35:27 +0200

    :art: level spinner, tweaks

--- projects/app-renderer/src/app/right-panel/right-panel.module.ts
+++ projects/app-renderer/src/app/right-panel/right-panel.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RightPanelComponent } from './right-panel.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [RightPanelComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule, MatCardModule],
   exports: [RightPanelComponent],
 })
 export class RightPanelModule {}
