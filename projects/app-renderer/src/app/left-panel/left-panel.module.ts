0ff65d8ed99c39e0fcfe1d7566c9daa679278fe7
Thomas Marcelin <thomas.marcelin.07@gmail.com>
2021-04-06 10:07:34 +0200

    :sparkles: static view of main content

--- projects/app-renderer/src/app/left-panel/left-panel.module.ts
+++ projects/app-renderer/src/app/left-panel/left-panel.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeftPanelComponent } from './left-panel.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LeftPanelComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatBadgeModule, MatButtonModule, MatIconModule],
   exports: [LeftPanelComponent],
 })
 export class LeftPanelModule {}
