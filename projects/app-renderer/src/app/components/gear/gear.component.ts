d23097cad9ec966eaff181f6cdf413342f45fab4
Thomas Marcelin <thomas.marcelin.07@gmail.com>
2021-07-22 18:01:54 +0200

    :wrench: updated gem data, cost, and origin

--- projects/app-renderer/src/app/components/gear/gear.component.ts
+++ projects/app-renderer/src/app/components/gear/gear.component.ts
@@ -1,9 +1,12 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
   OnChanges,
+  Output,
 } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { DialogService } from '../../services/dialog.service';
 import { Gem } from '../../types/gem';
 import { Link } from '../../types/link';
@@ -19,43 +22,98 @@ export class GearComponent implements OnChanges {
   @Input() public icon: string;
   @Input() public links: Link[];
   @Input() public gems: Gem[];
+  @Input() public questUsed: { name: string; quest: string }[];
+  @Output() public newQuestUsed: Subject<{ name: string; quest: string }> =
+    new Subject<{ name: string; quest: string }>();
 
   public rewardsList = [];
+  public vendorList = [];
 
-  constructor(private readonly dialogService: DialogService) {}
+  constructor(
+    private readonly dialogService: DialogService,
+    public cdr: ChangeDetectorRef
+  ) {}
 
   public ngOnChanges(): void {
     const className = this.dialogService.classSubject$.getValue();
-    this.rewardsList = this.gems.map((gem) => {
+    this.rewardsList = [];
+    this.gems.forEach((gem) => {
+      console.log('gem: ' + gem.gemName);
       const gemAssociated = gemData.find(
         (gemdata) =>
           gemdata.name
             .toLowerCase()
             .replace(/ /g, '')
-            .replace(/support/g, '') ===
+            .replace(/support/g, '')
+            // tslint:disable-next-line: quotemark
+            .replace('%27', "'") ===
           gem.gemName
             .toLowerCase()
             .replace(/ /g, '')
             .replace(/support/g, '')
       );
 
-      if (!gemAssociated?.classes.includes(className)) {
-        return {};
+      if (
+        gemAssociated?.questRewardClasses.includes(className) &&
+        !this.questUsed.find((q) => q.quest === gemAssociated.questReward)
+      ) {
+        console.log(gem.gemName + ' is not associated with quests');
+        this.newQuestUsed.next({
+          name: gem.gemName,
+          quest: gemAssociated.questReward,
+        });
+        this.rewardsList.push({
+          name: gem.gemName,
+          quest: gemAssociated.questReward,
+        });
+      } else if (
+        gemAssociated?.questRewardClasses.includes(className) &&
+        this.questUsed.find((q) => q.quest === gemAssociated.questReward)
+          .name === gem.gemName
+      ) {
+        console.log(gem.gemName + ' is associated with quests but it is ok');
+
+        this.rewardsList.push({
+          name: gem.gemName,
+          quest: gemAssociated.questReward,
+        });
       }
 
-      return {
+      if (!gemAssociated?.vendorClasses.includes(className)) {
+        this.vendorList.push({
+          name: gem.gemName,
+          cost: gemAssociated.gemCostUnit,
+        });
+      }
+
+      this.vendorList.push({
         name: gem.gemName,
-        npc: gemAssociated.npc,
-        quest: gemAssociated.questReward,
-      };
+        vendor: gemAssociated.vendor,
+        cost: gemAssociated.gemCostUnit,
+      });
     });
   }
 
-  public rewardSentence(gemName: string): string {
-    const rewards = this.rewardsList.find((reward) => reward.name === gemName);
-    if (!rewards) {
-      return null;
+  public rewardSentence(
+    rewardsList: any[],
+    vendorList: any[],
+    gemName: string
+  ): string {
+    console.log('reward', rewardsList);
+    const rewards = rewardsList.find((reward) => reward.name === gemName);
+    if (rewards) {
+      // tslint:disable-next-line: quotemark
+      return rewards.quest.replace('%27', "'");
     }
-    return `${rewards.npc} (${rewards.quest})`;
+
+    const quests = vendorList.find((reward) => reward.name === gemName);
+    if (quests) {
+      if (quests.vendor && quests.vendor !== '') {
+        // tslint:disable-next-line: quotemark
+        return `${quests.vendor} (${quests.cost.replace('%27', "'")})`;
+      }
+      // tslint:disable-next-line: quotemark
+      return `Siosa (${quests.cost.replace('%27', "'")})`;
+    }
   }
 }
--- projects/app-renderer/src/app/left-panel/left-panel.component.ts
+++ projects/app-renderer/src/app/left-panel/left-panel.component.ts
@@ -1,9 +1,11 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
   OnChanges,
 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { LevelingData } from '../types/leveling-data';
 import {
   findGearPiece,
@@ -26,4 +28,17 @@ export class LeftPanelComponent {
   public formatLinks = formatLinks;
   public formatGems = formatGems;
   public findGearPiece = findGearPiece;
+
+  public questUsedSubject = new BehaviorSubject<
+    { name: string; quest: string }[]
+  >([]);
+  public questUsed = this.questUsedSubject.asObservable();
+
+  constructor(private readonly cdr: ChangeDetectorRef) {}
+
+  public newQuestUsed(event: { name: string; quest: string }): void {
+    if (event?.name && event?.quest) {
+      this.questUsedSubject.next([...this.questUsedSubject.getValue(), event]);
+    }
+  }
 }
